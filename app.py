ed923679ec0f627349d18f6080cd197b57f32705
Dimitrije Dolga <ddimitrije@gmail.com>
2023-08-23 14:28:37 +0200

    pip upgrade4f

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def create_app(db_url=None):
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["PROPAGATE_EXCEPTIONS"] = True
+    #app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
     migrate = Migrate(app, db)
     api = Api(app)
